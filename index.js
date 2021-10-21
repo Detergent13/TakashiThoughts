80105e4edebade1075ab04b777b2b1883b8a5a4e
Detergent13 <dcrush@calpoly.edu>
2021-10-20 17:38:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const discordClient = new Discord.Client()
+const Twitter = require('twitter-api-v2')
+const twitterClient = new Twitter.TwitterApi()
+
+//setup - console output and activity set
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    discordClient.user.setActivity("The Transfer Demon Retcon 4.0", { type: "WATCHING"})
+})
+
+//ping
+discordClient.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!')
+    }
+})
+
+//toggles - exclusive to ID(s)
+discordClient.on('messageReactionAdd', reaction => {
+    if (reaction.id === process.env.EMOJI_ID && reaction.message.author.id === process.env.TAKASHI_ID && reaction.count >= process.env.REACT_THRESHOLD) {
+        if (reaction.message.content.length <= 280){
+            twitterClient.v1.tweet(reaction.message.content);
+            reaction.message.reply("Tweeted!")
+        }
+        else
+            reaction.message.reply("Message was too long for Twitter :(")
+    }
+})
+
+discordClient.login(process.env.BOT_TOKEN)
+twitterClient.login(process.env.TWITTER_TOKEN)
